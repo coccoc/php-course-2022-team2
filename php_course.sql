1725cbe7f4a7738589afebe951ab87b8c93556af
linhthuydanhbac <tuan.dc@urbox.vn>
2022-09-09 20:54:58 +0700

    db struct 🚀 🚀 🚀

--- php_course.sql
+++ php_course.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100425 (10.4.25-MariaDB-1:10.4.25+maria~focal)
  File Encoding         : 65001
 
- Date: 02/09/2022 20:29:50
+ Date: 09/09/2022 20:52:41
 */
 
 SET NAMES utf8mb4;
@@ -29,8 +29,18 @@ CREATE TABLE `booking` (
   `customer_name` varchar(255) NOT NULL COMMENT 'tên khách hàng',
   `shift` int(11) NOT NULL COMMENT 'ca khám: 1-morning, 2-afternoon',
   `status` int(10) unsigned DEFAULT 2 COMMENT 'trạng thái: 1-ẩn, 2-hiện',
+  `date` date DEFAULT NULL COMMENT 'ngày đặt lịch',
+  `schedule_id` int(11) DEFAULT NULL COMMENT 'mapping với bảng schedule',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of booking
+-- ----------------------------
+BEGIN;
+INSERT INTO `booking` (`id`, `doctor_id`, `customer_phone`, `customer_email`, `customer_name`, `shift`, `status`, `date`, `schedule_id`) VALUES (1, 1, '0168259116', 'tuandao864@gmail.com', 'John bosst', 2, 2, '2022-09-02', NULL);
+INSERT INTO `booking` (`id`, `doctor_id`, `customer_phone`, `customer_email`, `customer_name`, `shift`, `status`, `date`, `schedule_id`) VALUES (2, 1, '0168259116', 'tuandao864@gmail.com', 'John bosst', 1, 2, '2022-09-02', NULL);
+COMMIT;
 
 -- ----------------------------
 -- Table structure for doctor
@@ -44,7 +54,15 @@ CREATE TABLE `doctor` (
   `status` int(11) DEFAULT 2 COMMENT 'trạng thái: 1-ẩn, 2-hiện',
   `password` varchar(255) NOT NULL COMMENT 'mật khẩu đăng nhập tài khoản',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of doctor
+-- ----------------------------
+BEGIN;
+INSERT INTO `doctor` (`id`, `name`, `email`, `phone`, `status`, `password`) VALUES (1, 'kayla@example.com', '0', '0', 2, '0');
+INSERT INTO `doctor` (`id`, `name`, `email`, `phone`, `status`, `password`) VALUES (2, 'John bosst', 'tuandao864@gmail.com', '0168259116', 2, '123456789');
+COMMIT;
 
 -- ----------------------------
 -- Table structure for schedule
@@ -54,10 +72,17 @@ CREATE TABLE `schedule` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id tự tăng',
   `doctor_id` int(11) NOT NULL COMMENT 'tham chiếu doctor.id',
   `shift` int(11) NOT NULL COMMENT 'ca làm: 1-morning, 2-afternoon, 3-fulltime',
-  `date` int(11) NOT NULL COMMENT 'ngày làm, unix timestamp',
-  `booked` int(11) DEFAULT NULL COMMENT 'đã được book: 0- chưa có, 1-morning, 2-afternoon, 3-fulltime',
+  `date` date NOT NULL COMMENT 'ngày làm',
+  `booked` int(11) DEFAULT 0 COMMENT 'đã được book: 0- chưa có, 1-morning, 2-afternoon, 3-fulltime',
   `status` int(11) DEFAULT 2 COMMENT 'trạng thái: 1-ẩn, 2-hiện',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of schedule
+-- ----------------------------
+BEGIN;
+INSERT INTO `schedule` (`id`, `doctor_id`, `shift`, `date`, `booked`, `status`) VALUES (1, 1, 3, '2022-09-02', 3, 2);
+COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
